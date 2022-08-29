f07315302d7bb309990225ff1379dcd00ba58d73
tranhdq <tranhdq@hybrid-technologies.vn>
2022-08-29 16:42:34 +0700

    Implement Discount model

--- backend/ec_base/discount/filters/discount.py
+++ backend/ec_base/discount/filters/discount.py
@@ -1,7 +1,8 @@
-from ...common.constant.db_fields import CommonFields
+from ...common.constant.db_fields import CommonFields, DiscountFields, MasterFields
 
 
 class DiscountListQueryFields:
-    SEARCH_FIELDS = ()
-    ORDER_FIELDS = (CommonFields.ID,)
+    SEARCH_FIELDS = ("__".join([DiscountFields.DISCOUNT_TYPE, MasterFields.NAME]),
+                     "__".join([DiscountFields.DISCOUNT_RATE, MasterFields.NAME]))
+    ORDER_FIELDS = ("__".join([DiscountFields.DISCOUNT_TYPE, MasterFields.NAME]), CommonFields.ID,)
     ORDER_DEFAULT_FIELD = CommonFields.ID
